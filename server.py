272578278fdc6a4f9e411cc5b06ad4b2b88e49b1
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-04-04 14:45:19 +0300

    Upload notebook

--- server.py
+++ server.py
@@ -2,15 +2,16 @@ from flask import Flask, request, jsonify
 from mysql.connector import (connection)
 
 config = {
-  'user': 'onlineord',
-  'password': 'Bd4tO--2FL7V',
-  'host': 'den1.mysql4.gear.host',
-  'database': 'onlineord',
-  'raise_on_warnings': True
+    'user': 'onlineord',
+    'password': 'Bd4tO--2FL7V',
+    'host': 'den1.mysql4.gear.host',
+    'database': 'onlineord',
+    'raise_on_warnings': True
 }
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
 
 def get_users(cnx):
     cursor = cnx.cursor()
@@ -22,8 +23,9 @@ def get_users(cnx):
         'Surname': i[3],
         'Password': i[4],
         'Phone_number': i[5]} for i in cursor]
-    
+
     return res
+
 
 def get_user_by_id(cnx, id):
     cursor = cnx.cursor()
@@ -35,8 +37,9 @@ def get_user_by_id(cnx, id):
         'Surname': i[3],
         'Password': i[4],
         'Phone_number': i[5]} for i in cursor]
-    
+
     return res
+
 
 def get_user_by_email(cnx, email):
     cursor = cnx.cursor()
@@ -51,6 +54,7 @@ def get_user_by_email(cnx, email):
 
     return res
 
+
 def insert_user(cnx, email, password, name, surname, phone_number):
     cursor = cnx.cursor()
     cursor.execute(f"INSERT INTO users (Email, Passwords, Name, Surname, Phone_number) VALUES (\
@@ -61,6 +65,7 @@ def insert_user(cnx, email, password, name, surname, phone_number):
                         '{phone_number}')")
     cnx.commit()
 
+
 @app.route('/users/', methods=['GET'])
 def users():
     cnx = connection.MySQLConnection(**config)
@@ -68,12 +73,14 @@ def users():
 
     return jsonify(response)
 
+
 @app.route('/users/<int:user_id>', methods=['GET'])
 def user(user_id):
     cnx = connection.MySQLConnection(**config)
     response = get_user_by_id(cnx, user_id)
 
     return jsonify(response)
+
 
 @app.route('/users/', methods=['POST'])
 def login():
@@ -84,7 +91,7 @@ def login():
         'email': request.form['email'],
         'password': request.form['password']
     }
-    
+
     cursor.execute(f"SELECT * FROM users WHERE Email = '{req['email']}'")
 
     if(cursor.rowcount == 1):
@@ -95,28 +102,29 @@ def login():
             'Surname': i[3],
             'Password': i[4],
             'Phone_number': i[5]} for i in cursor]
-        
+
         if(user[0]['Password'] == req['password']):
             response = user
         else:
             response = [{
-            'error': {
-                'code': 1,
-                'description': "invalid password",
-                'data': req
-            }
-        }]
+                'error': {
+                    'code': 1,
+                    'description': "invalid password",
+                    'form-params': req
+                }
+            }]
     else:
         response = [{
             'error': {
                 'code': 0,
                 'description': "invalid email",
-                'data': req
+                'form-params': req
             }
         }]
 
     cnx.close()
     return jsonify(response)
+
 
 @app.route('/users/new', methods=['POST'])
 def registration():
@@ -130,27 +138,30 @@ def registration():
         'surname': request.form['surname'],
         'phone_number': request.form['phone_number']
     }
-    
+
     cursor.execute(f"SELECT * FROM users WHERE Email = '{req['email']}'")
 
     if(cursor.rowcount == 0):
-        insert_user(cnx, req['email'], req['password'], req['name'], req['surname'], req['phone_number'])
+        insert_user(cnx, req['email'], req['password'],
+                    req['name'], req['surname'], req['phone_number'])
         response = get_user_by_email(cnx, req['email'])
     else:
         response = [{
             'error': {
                 'code': 0,
                 'description': "email already exists",
-                'data': req
+                'form-params': req
             }
         }]
 
     cnx.close()
     return jsonify(response)
 
+
 @app.route("/")
 def home():
     return "OnlineOrders server works!"
+
 
 if __name__ == "__main__":
     app.run(threaded=True, port=5000)
