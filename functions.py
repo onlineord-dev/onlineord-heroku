669ef82864b4435708ed5a018144beb1f2d0a746
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-04-08 15:24:52 +0300

    Import functions

---
+++ functions.py
@@ -0,0 +1,51 @@
+def get_users(cnx):
+    cursor = cnx.cursor()
+    cursor.execute("SELECT * FROM users")
+    res = [{
+        'ID': i[0],
+        'Email': i[1],
+        'Name': i[2],
+        'Surname': i[3],
+        'Password': i[4],
+        'Phone_number': i[5]} for i in cursor]
+
+    return res
+
+
+def get_user_by_id(cnx, id):
+    cursor = cnx.cursor()
+    cursor.execute(f"SELECT * FROM users WHERE id = '{id}'")
+    res = [{
+        'ID': i[0],
+        'Email': i[1],
+        'Name': i[2],
+        'Surname': i[3],
+        'Password': i[4],
+        'Phone_number': i[5]} for i in cursor]
+
+    return res
+
+
+def get_user_by_email(cnx, email):
+    cursor = cnx.cursor()
+    cursor.execute(f"SELECT * FROM users WHERE Email = '{email}'")
+    res = [{
+        'ID': i[0],
+        'Email': i[1],
+        'Name': i[2],
+        'Surname': i[3],
+        'Password': i[4],
+        'Phone_number': i[5]} for i in cursor]
+
+    return res
+
+
+def insert_user(cnx, email, password, name, surname, phone_number):
+    cursor = cnx.cursor()
+    cursor.execute(f"INSERT INTO users (Email, Passwords, Name, Surname, Phone_number) VALUES (\
+                        '{email}',\
+                        '{password}',\
+                        '{name}',\
+                        '{surname}',\
+                        '{phone_number}')")
+    cnx.commit()
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from mysql.connector import (connection)
 
+import functions as db
+
 config = {
     'user': 'onlineord',
     'password': 'Bd4tO--2FL7V',
@@ -13,63 +15,10 @@ app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
-def get_users(cnx):
-    cursor = cnx.cursor()
-    cursor.execute("SELECT * FROM users")
-    res = [{
-        'ID': i[0],
-        'Email': i[1],
-        'Name': i[2],
-        'Surname': i[3],
-        'Password': i[4],
-        'Phone_number': i[5]} for i in cursor]
-
-    return res
-
-
-def get_user_by_id(cnx, id):
-    cursor = cnx.cursor()
-    cursor.execute(f"SELECT * FROM users WHERE id = '{id}'")
-    res = [{
-        'ID': i[0],
-        'Email': i[1],
-        'Name': i[2],
-        'Surname': i[3],
-        'Password': i[4],
-        'Phone_number': i[5]} for i in cursor]
-
-    return res
-
-
-def get_user_by_email(cnx, email):
-    cursor = cnx.cursor()
-    cursor.execute(f"SELECT * FROM users WHERE Email = '{email}'")
-    res = [{
-        'ID': i[0],
-        'Email': i[1],
-        'Name': i[2],
-        'Surname': i[3],
-        'Password': i[4],
-        'Phone_number': i[5]} for i in cursor]
-
-    return res
-
-
-def insert_user(cnx, email, password, name, surname, phone_number):
-    cursor = cnx.cursor()
-    cursor.execute(f"INSERT INTO users (Email, Passwords, Name, Surname, Phone_number) VALUES (\
-                        '{email}',\
-                        '{password}',\
-                        '{name}',\
-                        '{surname}',\
-                        '{phone_number}')")
-    cnx.commit()
-
-
 @app.route('/users/', methods=['GET'])
 def users():
     cnx = connection.MySQLConnection(**config)
-    response = get_users(cnx)
+    response = db.get_users(cnx)
 
     return jsonify(response)
 
@@ -77,7 +26,7 @@ def users():
 @app.route('/users/<int:user_id>', methods=['GET'])
 def user(user_id):
     cnx = connection.MySQLConnection(**config)
-    response = get_user_by_id(cnx, user_id)
+    response = db.get_user_by_id(cnx, user_id)
 
     return jsonify(response)
 
@@ -142,9 +91,9 @@ def registration():
     cursor.execute(f"SELECT * FROM users WHERE Email = '{req['email']}'")
 
     if(cursor.rowcount == 0):
-        insert_user(cnx, req['email'], req['password'],
+        db.insert_user(cnx, req['email'], req['password'],
                     req['name'], req['surname'], req['phone_number'])
-        response = get_user_by_email(cnx, req['email'])
+        response = db.get_user_by_email(cnx, req['email'])
     else:
         response = [{
             'error': {
@@ -156,6 +105,12 @@ def registration():
 
     cnx.close()
     return jsonify(response)
+
+
+@app.route('/menu', methods=['POST'])
+def get_menu():
+
+    return 0
 
 
 @app.route("/")
